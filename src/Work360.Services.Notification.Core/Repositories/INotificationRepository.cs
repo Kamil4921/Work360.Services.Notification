2b4954ede0cdb986ae02943221e3f964279b2ef9
kamil4921 <kamil.j4921@gmail.com>
2024-12-11 21:22:22 +0100

    Corrected typo

--- src/Work360.Services.Notification.Core/Repositories/INotificationRepository.cs
+++ src/Work360.Services.Notification.Core/Repositories/INotificationRepository.cs
@@ -3,7 +3,7 @@ namespace Work360.Services.Notification.Core.Repositories;
 public interface INotificationRepository
 {
     Task<Entities.Notification> GetNotification(Guid id);
-    Task<IEnumerable<Entities.Notification>> GetNotification();
+    Task<IEnumerable<Entities.Notification>> GetNotifications();
     Task AddNotification(Entities.Notification notification);
     Task UpdateNotification(Entities.Notification notification);
     Task DeleteNotification(Guid id);
