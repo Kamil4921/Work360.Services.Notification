5d7ffcd68068ff944413f84f5faeeee856f33f4d
kamil4921 <kamil.j4921@gmail.com>
2024-12-12 19:52:36 +0100

    Created Command handlers

--- src/Work360.Services.Notification.Application/Commands/Handlers/CreateNotificationHandler.cs
+++ src/Work360.Services.Notification.Application/Commands/Handlers/CreateNotificationHandler.cs
@@ -1,11 +1,20 @@
 using MediatR;
+using Work360.Services.Notification.Application.Services;
+using Work360.Services.Notification.Core.Repositories;
 
 namespace Work360.Services.Notification.Application.Commands.Handlers;
 
-public class CreateNotificationHandler : IRequestHandler<CreateNotification, Guid>
+public class CreateNotificationHandler(INotificationRepository notificationRepository, IEventMapper eventMapper, IMessageBroker messageBroker) : IRequestHandler<CreateNotification, Guid>
 {
-    public Task<Guid> Handle(CreateNotification request, CancellationToken cancellationToken)
+    public async Task<Guid> Handle(CreateNotification request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var notification = Core.Entities.Notification.CreateNotification(request.Email, request.Title, request.Text);
+        var adding = notificationRepository.AddNotification(notification);
+        var events = eventMapper.MapAll(notification.Events);
+        var publishing = messageBroker.PublishAsync(events!);
+
+        await Task.WhenAll(adding, publishing);
+
+        return notification.Id;
     }
 }
--- src/Work360.Services.Notification.Application/Commands/Handlers/SendNotificationHandler.cs
+++ src/Work360.Services.Notification.Application/Commands/Handlers/SendNotificationHandler.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using MediatR;
 
 namespace Work360.Services.Notification.Application.Commands.Handlers;
@@ -6,6 +7,18 @@ public class SendNotificationHandler : IRequestHandler<SendNotification, Guid>
 {
     public Task<Guid> Handle(SendNotification request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var client = new SmtpClient("localhost");
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("work360@work360.com"),
+            Subject = request.Notification.Title,
+            Body = request.Notification.Text,
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(request.Notification.Email);
+        client.Send(mailMessage);
+
+        return Task.FromResult(request.Notification.Id);
     }
 }
