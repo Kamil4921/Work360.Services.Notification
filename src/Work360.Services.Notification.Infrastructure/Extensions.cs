bcda451fc933f17e042bcd035ba6afcdc506de8d
kamil4921 <kamil.j4921@gmail.com>
2024-12-29 17:57:34 +0100

    Register NotificationRepository

--- src/Work360.Services.Notification.Infrastructure/Extensions.cs
+++ src/Work360.Services.Notification.Infrastructure/Extensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
 using Work360.Services.Notification.Application.Services;
+using Work360.Services.Notification.Core.Repositories;
 using Work360.Services.Notification.Infrastructure.CosmosDb;
 using Work360.Services.Notification.Infrastructure.Services;
 
@@ -10,8 +11,9 @@ public static class Extensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddSingleton<CosmosClient>(sp => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
+        services.AddSingleton<CosmosClient>(_ => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
         services.AddTransient<ISendService, SendService>();
+        services.AddTransient<INotificationRepository, NotificationRepository>();
         
         services.AddScoped(sp => new CosmosDbContext(sp.GetRequiredService<CosmosClient>(), "work360-Notification", "notifications"));
         
