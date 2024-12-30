1a2c5813910b82ec37dba1ba9a1ac5c4470f0307
kamil4921 <kamil.j4921@gmail.com>
2024-12-30 21:31:37 +0100

    removed email

--- src/Work360.Services.Notification.Application/DTO/NotificationDto.cs
+++ src/Work360.Services.Notification.Application/DTO/NotificationDto.cs
@@ -2,7 +2,6 @@ namespace Work360.Services.Notification.Application.DTO;
 
 public record NotificationDto(Core.Entities.Notification notification)
 {
-    public string Email { get; } = notification.Email;
     public string Text { get; } = notification.Text;
     public string Title { get; } = notification.Title;
 }
--- src/Work360.Services.Notification.Core/Entities/Notification.cs
+++ src/Work360.Services.Notification.Core/Entities/Notification.cs
@@ -1,38 +1,25 @@
 using Work360.Services.Notification.Core.Events;
-using Work360.Services.Notification.Core.Exceptions;
-
 namespace Work360.Services.Notification.Core.Entities;
 
 public class Notification : AggregateRoot
 {
-    public string Email { get; set; }
     public string Text { get; set; }
     public string Title { get; set; } 
     public DateTime TimeStamp { get; set; }
 
-    private Notification(string email, string title, string text)
+    private Notification(string title, string text)
     {
         Id = Guid.NewGuid();
-        Email = email;
         Text = text;
         Title = title;
         TimeStamp = DateTime.Now;
     }
     
-    public static Notification CreateNotification(string email, string title, string text)
+    public static Notification CreateNotification(string title, string text)
     {
-        CheckEmail(email);
-        var notification = new Notification(email, title, text);
+        var notification = new Notification(title, text);
         notification.AddEvent(new NotificationCreated(notification));
 
         return notification;
-    }
-
-    private static void CheckEmail(string email)
-    {
-        if (!email.Contains('@'))
-        {
-            throw new InvalidEmailException(email);
-        }
     }
 }
