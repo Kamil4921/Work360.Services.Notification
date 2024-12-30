0c9c517f6284c31c086cb2cfb34b5610c55c871b
kamil4921 <kamil.j4921@gmail.com>
2024-12-30 21:25:56 +0100

    Created Service bus receiver

---
+++ src/Work360.Services.Notification.Infrastructure/Services/ServiceBusMessageReceiver.cs
@@ -0,0 +1,41 @@
+using Azure.Messaging.ServiceBus;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Work360.Services.Notification.Application.Commands;
+using Work360.Services.Notification.Application.DTO;
+
+namespace Work360.Services.Notification.Infrastructure.Services;
+
+public class ServiceBusMessageReceiver (ISender mediator, ILogger<ServiceBusMessageReceiver> logger)
+{
+    private const string ConnectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
+    private const string TopicName = "employee-topic";
+    private const string SubscriptionName = "subscription.4";
+
+    public async Task StartAsync()
+    {
+        var client = new ServiceBusClient(ConnectionString);
+        var processor = client.CreateProcessor(TopicName, SubscriptionName, new ServiceBusProcessorOptions());
+
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+        logger.LogInformation("Starting service bus");
+        await processor.StartProcessingAsync();
+    }
+
+    private async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        var leaves = JsonConvert.DeserializeObject<List<LeaveDto>>(args.Message.Body.ToString())?? throw new ArgumentNullException();
+        var leave = leaves.First();
+        await mediator.Send(new CreateLeave(leave.LeaveId, leave.EmployeeFullName, leave.LeaveStart, leave.LeaveDuration));
+        await mediator.Send(new CreateNotification($"{leave.EmployeeFullName} took leave from {leave.LeaveStart.Date} to {leave.LeaveStart.Date.AddDays(leave.LeaveDuration)}", $"{leave.EmployeeFullName} on leave"));
+    }
+
+    private Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        logger.LogError("Error occured in the topic");
+        //TODO:Add monitoring
+        return Task.CompletedTask;
+    }
+}
