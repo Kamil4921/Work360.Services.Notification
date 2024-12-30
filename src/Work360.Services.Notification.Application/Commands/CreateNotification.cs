531066935014fef0db7ce6ab921f2fc24718bbdf
kamil4921 <kamil.j4921@gmail.com>
2024-12-30 21:29:13 +0100

    removed email prop

--- src/Work360.Services.Notification.Application/Commands/CreateNotification.cs
+++ src/Work360.Services.Notification.Application/Commands/CreateNotification.cs
@@ -3,9 +3,8 @@ using MediatR;
 namespace Work360.Services.Notification.Application.Commands;
 
 [Contract]
-public class CreateNotification(string email, string text, string title) : IRequest<Guid>
+public class CreateNotification(string text, string title) : IRequest<Guid>
 {
-    public string Email { get; } = email;
     public string Text { get; } = text;
     public string Title { get; } = title;
 }
--- src/Work360.Services.Notification.Application/Commands/Handlers/CreateNotificationHandler.cs
+++ src/Work360.Services.Notification.Application/Commands/Handlers/CreateNotificationHandler.cs
@@ -4,16 +4,17 @@ using Work360.Services.Notification.Core.Repositories;
 
 namespace Work360.Services.Notification.Application.Commands.Handlers;
 
-public class CreateNotificationHandler(INotificationRepository notificationRepository, IEventMapper eventMapper, IMessageBroker messageBroker) : IRequestHandler<CreateNotification, Guid>
+public class CreateNotificationHandler(INotificationRepository notificationRepository, IEventMapper eventMapper, IMessageBroker messageBroker, IMediator mediator) : IRequestHandler<CreateNotification, Guid>
 {
     public async Task<Guid> Handle(CreateNotification request, CancellationToken cancellationToken)
     {
-        var notification = Core.Entities.Notification.CreateNotification(request.Email, request.Title, request.Text);
+        var notification = Core.Entities.Notification.CreateNotification(request.Title, request.Text);
+        var send = mediator.Send(new SendNotification(notification));
         var adding = notificationRepository.AddNotification(notification);
         var events = eventMapper.MapAll(notification.Events);
         var publishing = messageBroker.PublishAsync(events!);
 
-        await Task.WhenAll(adding, publishing);
+        await Task.WhenAll(adding, publishing, send);
 
         return notification.Id;
     }
