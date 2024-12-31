bc9701e6a21b54aaee6f0eba3e75f443eb32a642
kamil4921 <kamil.j4921@gmail.com>
2024-12-31 15:20:54 +0100

    Change in DI due to container change

--- src/Work360.Services.Notification.Infrastructure/Extensions.cs
+++ src/Work360.Services.Notification.Infrastructure/Extensions.cs
@@ -14,8 +14,9 @@ public static class Extensions
         services.AddSingleton<CosmosClient>(_ => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
         services.AddTransient<ISendService, SendService>();
         services.AddTransient<INotificationRepository, NotificationRepository>();
+        services.AddTransient<ILeaveRepository, LeaveRepository>();
         
-        services.AddScoped(sp => new CosmosDbContext(sp.GetRequiredService<CosmosClient>(), "work360-Notification", "notifications"));
+        services.AddScoped(sp => new CosmosDbContext(sp.GetRequiredService<CosmosClient>(), "work360-Notification"));
         
         return services;
     }
