00f14df5e89a3c71f484765922f48ba7d9b614d7
kamil4921 <kamil.j4921@gmail.com>
2025-01-01 17:26:57 +0100

    Implemented query handlers

--- src/Work360.Services.Notification.Application/Queries/Handlers/GetLeaveHandler.cs
+++ src/Work360.Services.Notification.Application/Queries/Handlers/GetLeaveHandler.cs
@@ -1,6 +1,22 @@
+using MediatR;
+using Work360.Services.Notification.Application.DTO;
+using Work360.Services.Notification.Application.Exceptions;
+using Work360.Services.Notification.Core.Repositories;
+
 namespace Work360.Services.Notification.Application.Queries.Handlers;
 
-public class GetLeaveHandler
+public class GetLeaveHandler(ILeaveRepository leaveRepository) : IRequestHandler<GetLeave, LeaveDto>
 {
-    
+    public async Task<LeaveDto> Handle(GetLeave request, CancellationToken cancellationToken)
+    {
+        var leave = await leaveRepository.GetLeaveAsync(request.LeaveId);
+        if (leave is null)
+        {
+            throw new LeaveNotFoundException(request.LeaveId);
+        }
+
+        var leaveDto = new LeaveDto(leave.Id, leave.EmployeeFullName, leave.LeaveStart, leave.LeaveDuration);
+
+        return leaveDto;
+    }
 }
--- src/Work360.Services.Notification.Application/Queries/Handlers/GetLeavesHandler.cs
+++ src/Work360.Services.Notification.Application/Queries/Handlers/GetLeavesHandler.cs
@@ -1,6 +1,17 @@
+using MediatR;
+using Work360.Services.Notification.Application.DTO;
+using Work360.Services.Notification.Core.Repositories;
+
 namespace Work360.Services.Notification.Application.Queries.Handlers;
 
-public class GetLeavesHandler
+public class GetLeavesHandler(ILeaveRepository leaveRepository) : IRequestHandler<GetLeaves, IEnumerable<LeaveDto>>
 {
-    
+    public async Task<IEnumerable<LeaveDto>> Handle(GetLeaves request, CancellationToken cancellationToken)
+    {
+        var leaves = await leaveRepository.GetLeavesAsync();
+        var leavesDto = leaves.Select(leave =>
+            new LeaveDto(leave.Id, leave.EmployeeFullName, leave.LeaveStart, leave.LeaveDuration)).ToList();
+
+        return leavesDto;
+    }
 }
