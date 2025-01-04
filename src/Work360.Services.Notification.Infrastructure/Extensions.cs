f3b591803e7be524f4645f2c346eadf7fdcb07bc
kamil4921 <kamil.j4921@gmail.com>
2025-01-04 21:03:35 +0100

    register message broker and event mapper in di container

--- src/Work360.Services.Notification.Infrastructure/Extensions.cs
+++ src/Work360.Services.Notification.Infrastructure/Extensions.cs
@@ -11,10 +11,13 @@ public static class Extensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddSingleton<IEventMapper, EventMapper>();
+        services.AddSingleton<IMessageBroker, MessageBroker>();
         services.AddSingleton<CosmosClient>(_ => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
         services.AddTransient<ISendService, SendService>();
         services.AddTransient<INotificationRepository, NotificationRepository>();
         services.AddTransient<ILeaveRepository, LeaveRepository>();
+        services.AddSingleton<ServiceBusMessageReceiver>();
         
         services.AddScoped(sp => new CosmosDbContext(sp.GetRequiredService<CosmosClient>(), "work360-Notification"));
         
