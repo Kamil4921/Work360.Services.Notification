d18fd2103130caa0d895671b6b43c0b6e57803be
kamil4921 <kamil.j4921@gmail.com>
2025-01-08 18:11:16 +0100

    Added options for CosmosClient for fixing certificate problem

--- src/Work360.Services.Notification.Infrastructure/Extensions.cs
+++ src/Work360.Services.Notification.Infrastructure/Extensions.cs
@@ -11,9 +11,23 @@ public static class Extensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        var options = new CosmosClientOptions
+        {
+            HttpClientFactory = () =>
+            {
+                HttpMessageHandler httpMessageHandler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback =
+                        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+                return new HttpClient(httpMessageHandler);
+            },
+            ConnectionMode = ConnectionMode.Gateway,
+            LimitToEndpoint = true
+        };
         services.AddSingleton<IEventMapper, EventMapper>();
         services.AddSingleton<IMessageBroker, MessageBroker>();
-        services.AddSingleton<CosmosClient>(_ => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
+        services.AddSingleton<CosmosClient>(_ => new CosmosClient("https://localhost:2500", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", options));
         services.AddTransient<ISendService, SendService>();
         services.AddTransient<INotificationRepository, NotificationRepository>();
         services.AddTransient<ILeaveRepository, LeaveRepository>();
@@ -23,4 +37,6 @@ public static class Extensions
         
         return services;
     }
+    
+    //
 }
