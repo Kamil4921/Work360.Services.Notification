b41f28d0cabdfc8580232510537f829d3eb8779b
kamil4921 <kamil.j4921@gmail.com>
2025-01-08 18:10:41 +0100

    Adjustment AggregateRoot for serialization problems

--- src/Work360.Services.Notification.Core/Entities/AggregateRoot.cs
+++ src/Work360.Services.Notification.Core/Entities/AggregateRoot.cs
@@ -1,9 +1,16 @@
+using Newtonsoft.Json;
+
 namespace Work360.Services.Notification.Core.Entities;
 
 public class AggregateRoot
 {
+    [JsonProperty("id")]
     public Guid Id { get; set; }
+    
+    [JsonIgnore]
     private readonly List<IDomainEvent> _events = new();
+    
+    [JsonIgnore]
     public IEnumerable<IDomainEvent> Events => _events;
     public int Version { get; set; }
     protected void AddEvent(IDomainEvent @event)
